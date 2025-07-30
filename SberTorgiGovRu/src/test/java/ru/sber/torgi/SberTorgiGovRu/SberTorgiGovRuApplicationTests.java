377672b8dabaab307263c4380402c2f1e6dd2bd5
48-iq <ivn25ov@mail.ru>
2025-07-30 07:42:44 +0400

    fix tests

--- SberTorgiGovRu/src/test/java/ru/sber/torgi/SberTorgiGovRu/SberTorgiGovRuApplicationTests.java
+++ SberTorgiGovRu/src/test/java/ru/sber/torgi/SberTorgiGovRu/SberTorgiGovRuApplicationTests.java
@@ -3,7 +3,7 @@ package ru.sber.torgi.SberTorgiGovRu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SberTorgiGovRuApplicationTests {
 
 	@Test
